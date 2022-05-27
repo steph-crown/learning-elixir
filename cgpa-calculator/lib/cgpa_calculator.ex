94953d6499a17579be13a8696be915c828905e46
steph-crown <emmanuelstephen024@gmail.com>
2022-05-27 12:31:33 +0100

    Update the cgpa calculator

--- cgpa-calculator/lib/cgpa_calculator.ex
+++ cgpa-calculator/lib/cgpa_calculator.ex
@@ -2,10 +2,11 @@ defmodule CgpaCalculator do
   @course_list []
   # [course_list]
 
-  def add_course(name, score, unit) do
-    ori = @course_list
-    IO.inspect(ori)
-    @course_list = ori ++ [{"name", "score", "unit"}]
+  def add_course() do
+    namer = IO.gets("Enter course name\t")
+    scorer = IO.gets("Enter score\t")
+    unitr = IO.gets("Enter unit\t")
+    %{name: namer, score: scorer, unitr: unitr}
   end
 
   def get_course() do
@@ -22,13 +23,13 @@ defmodule CgpaCalculator do
   # Gets the grade of a score
   def get_score_grade(score) do
     cond do
-      0 < score and score < 40 -> 0
-      40 <= score and score < 45 -> 1
-      45 <= score and score < 50 -> 2
-      50 <= score and score < 60 -> 3
-      60 <= score and score < 70 -> 4
-      70 <= score and score <= 100 -> 5
-      true -> -1
+      0 < score and score < 40 -> {:ok, 0}
+      40 <= score and score < 45 -> {:ok, 1}
+      45 <= score and score < 50 -> {:ok, 2}
+      50 <= score and score < 60 -> {:ok, 3}
+      60 <= score and score < 70 -> {:ok, 4}
+      70 <= score and score <= 100 -> {:ok, 5}
+      true -> {:error, "Score is not between 1 to 100"}
     end
   end
 
